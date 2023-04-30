755443cfdb6d5f72619ac4fd6b64de8423296ba7
Tracie35 <kariukitracie@gmail.com>
2023-04-30 15:23:20 +0300

    added alist of anime

--- random_animes.py
+++ random_animes.py
@@ -1,7 +1,18 @@
 from random import choice 
 
 #list of animes
-animes = ['naruto','bleach','classroom assasin','one piece']
+
+animes = [['naruto','bleach','classroom assasin','one piece','series']
+          ['Fairy Tail','Black Clover','Code Geass','Sailor Moon','series']
+          ['Dragon Ball Z','Death Note','Attack on Titan','My Hero Academia','series']]
 
 #print random anime from list
 print(choice(animes))
+
+#mood
+print('what mood are you in?' )
+mood=input()
+
+#loop through and find matching mood 
+
+
